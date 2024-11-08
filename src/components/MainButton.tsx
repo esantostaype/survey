895083ac59642b5d2a325e43aa9ee0fbe5a09a55
Erick <esantostaype@gmail.com>
2024-11-08 12:16:43 -0500

    button arrow

--- src/components/MainButton.tsx
+++ src/components/MainButton.tsx
@@ -12,7 +12,9 @@ export const MainButton = ({ label, onClick }: Props) => {
         className='flex items-center gap-2 text-lg text-black py-3 px-8 uppercase group rounded-[4rem] font-bold bg-gradient-to-r to-accent1 from-accent2'
       >
         { label }
-        <ArrowForwardRoundedIcon className='relative group-hover:ml-2 group-hover:-mr-2 transition-all' />
+        <div className='relative group-hover:ml-2 group-hover:-mr-2 transition-all'>
+          <ArrowForwardRoundedIcon/>
+        </div>
       </button>
   );
 }
--- src/components/MainButtonLink.tsx
+++ src/components/MainButtonLink.tsx
@@ -15,7 +15,9 @@ export const MainButtonLink = ({ label, path }: Props) => {
         className='flex items-center gap-2 text-lg text-black py-3 px-8 uppercase group rounded-[4rem] font-bold bg-gradient-to-r to-accent1 from-accent2'
       >
         { label }
-        <ArrowForwardRoundedIcon className='relative group-hover:ml-2 group-hover:-mr-2 transition-all' />
+        <div className='relative group-hover:ml-2 group-hover:-mr-2 transition-all'>
+          <ArrowForwardRoundedIcon/>
+        </div>
       </button>
   );
 }
